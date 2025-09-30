975ef98e81b32eec408249ab5033db8c4a88f0b4
Baptiste Lyet <44911483+Babali42@users.noreply.github.com>
2025-09-30 14:49:49 +0200

    Add few tests

--- src/01_about_streams.test.ts
+++ src/01_about_streams.test.ts
@@ -1,4 +1,5 @@
-import {of} from "rxjs";
+import {of, Subject} from "rxjs";
+import {fromArrayLike} from "rxjs/internal/observable/innerFrom";
 
 var __ = 'Fill in the blank';
 
@@ -16,50 +17,55 @@ describe("", () => {
             done();
         });
     });
+
+    it("Should subscribe to a subject",  (done: jest.DoneCallback)  => {
+        var events = new Subject();
+        events.subscribe(function (x) { expect(x).toBe(__); done(); });
+        events.next(37);
+    });
+
+    it("how event streams relate to observables", (done: jest.DoneCallback) => {
+        let observableResult = 1;
+        of(73).subscribe(x => { observableResult = x; });
+
+        let eventStreamResult = 1;
+        var events = new Subject();
+        //@ts-ignore
+        events.subscribe(x => eventStreamResult = x);
+        //events.next(73);
+
+        expect(observableResult).toEqual(eventStreamResult);
+        done();
+    })
+
+    it("events with multiples values", (done: jest.DoneCallback) => {
+        var eventStreamResult = 0;
+        var events = new Subject();
+
+        //@ts-ignore
+        events.subscribe(function (x) { eventStreamResult += x; });
+
+        events.next(10);
+        events.next(7);
+
+        expect(eventStreamResult).toEqual(__);
+        done();
+    })
+
+    it("", (done: jest.DoneCallback) => {
+        test('the last event', function () {
+            var received = '';
+            var names = ['foo', 'bar'];
+            fromArrayLike(names).subscribe(function (x) {
+                received = x;
+            });
+
+            expect(received).toEqual('bar');
+            done();
+        });
+    });
 });
 
-// test('what comes in goes out', function () {
-//   Observable.just(101).subscribe(function (x) { equal(x, 101); });
-// });
-//
-// // Which interface Rx apply? (hint: what does "just()" return)
-// test('this is the same as an event stream', function () {
-//   var events = new Subject();
-//   events.subscribe(function (x) { equal(37, x); });
-//   events.onNext(37);
-// });
-//
-// // What is the relationship between "this is the same as an event stream" and "simple subscription"?
-// test('how event streams relate to observables', function () {
-//   var observableResult = 1;
-//   Observable.just(73).subscribe(function (x) { observableResult = x; });
-//
-//   var eventStreamResult = 1;
-//   var events = new Subject();
-//   events.subscribe(function (x) { eventStreamResult = x; });
-//   events.onNext(73);
-//
-//   equal(observableResult, eventStreamResult);
-// });
-//
-// test('event streams have multiple results', function () {
-//   var eventStreamResult = 0;
-//   var events = new Subject();
-//   events.subscribe(function (x) { eventStreamResult += x; });
-//
-//   events.onNext(10);
-//   events.onNext(7);
-//
-//   equal(17, eventStreamResult);
-// });
-//
-// // What does Observable.just() map to for a Subject?
-// test('simple return', function () {
-//   var received = '';
-//   Observable.just('foo').subscribe(function (x) { received = x; });
-//
-//   equal("foo", received);
-// });
 //
 // test('the last event', function () {
 //   var received = '';
